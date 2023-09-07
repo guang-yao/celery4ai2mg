a3f9b981d827b4ddc47f0ca9155bd7016c9e54d2
yaoguang <yaoguang>
2023-09-07 16:32:36 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from . import celery_app, task_name_dict
 def task_async(func_name,datas,priority=5):
     task = celery_app.send_task(task_name_dict[func_name]['task_name'], args=[datas,], kwargs={},priority=priority)
     soft_time_limit = task_name_dict[func_name].get('soft_time_limit', 120)
-    if soft_time_limit is None: soft_time_limit == 120
+    if soft_time_limit is None: soft_time_limit = 120
     cost = 0
     while cost < soft_time_limit:
         task = AsyncResult(task.id)
