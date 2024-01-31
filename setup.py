28b0cc694a0d1d637e9c55c56662abafab9eeb5d
yaoguang <yaoguang>
2024-01-31 15:10:19 +0800

    __init__.py

--- setup.py
+++ setup.py
@@ -1,14 +1,22 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='celery4ai2mg',
-    version='1.0.0',
-    author='yaoguang',
-    author_email='yaoguang@mgtv.com',
-    description='快速构建celery异步任务',
-    py_modules=['utils','tasks'],
+    name="celery4ai2mg",
+    version="0.0.1",
+    keywords=("celery", "mgtv"),
+    description="快速构建celery异步任务",
+    long_description="long_description",
+    license="MIT Licence",
+
+    url="https://git.imgo.tv/aiops/celery_ai2mg.git@package",
+    author="yaoguang",
+    author_email="yaoguang@mgtv.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
     install_requires=[
         'celery==5.2.7',
-        'celery-amqp-backend==1.0.0',
-    ],
+        'celery-amqp-backend==1.0.0'
+        ]
 )
